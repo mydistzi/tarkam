aa775a8b6fac979d69ff88ab7e1658a2d2deef10
mydistzi <mysurellinux@gmail.com>
2026-04-05 05:37:42 +0700

    change endpoint api base url

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -3,9 +3,7 @@ import axios from "axios";
 
 const Api = axios.create({
   // Set default endpoint API
-  // baseURL: "https://tarkam-api.sintekp.com/api/v1",
   // baseURL: "http://127.0.0.1:8000/api/v1",
-  // baseURL: "https://tarkam-api.primatechindo.com/api/v1",
   baseURL: "https://tarkam-api-web-production.up.railway.app/api/v1",
 });
 
