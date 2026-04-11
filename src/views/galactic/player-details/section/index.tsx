32517653a07f2c710657273681f399e08d181742
mydistzi <mysurellinux@gmail.com>
2026-04-11 18:57:11 +0700

    Update timeline section heading in PlayerDetailsContent to improve styling

--- src/views/galactic/player-details/section/index.tsx
+++ src/views/galactic/player-details/section/index.tsx
@@ -193,7 +193,7 @@ const PlayerDetailsContent = ({
         <div className="container">
           <div className="section-heading mb-30 text-center">
             <h3>Timeline Karier</h3>
-            <h3>Perjalanan <span>{player.name}</span> selama season ini.</h3>
+            <h2>Perjalanan <span>{player.name}</span> selama season ini.</h2>
           </div>
           <div className="row cart-body pb-30">
             {record.timeline.map((entry: { label: string; value: string }) => (
