c58015ce4beeb4efd420f5cf12eaec3d87168de0
mydistzi <mysurellinux@gmail.com>
2026-04-12 12:47:20 +0700

    Remove TestimonialSection and related player data from SponsorsContent for code simplification

--- src/views/galactic/sponsors/section/index.tsx
+++ src/views/galactic/sponsors/section/index.tsx
@@ -1,15 +1,9 @@
-import { PageHeader, SponsorGrid, TestimonialSection } from "@/galactic/common";
-import { players, type SponsorItem } from "@/galactic/data";
+import { PageHeader, SponsorGrid } from "@/galactic/common";
+import { type SponsorItem } from "@/galactic/data";
 
 type SponsorsContentProps = {
   sponsors: SponsorItem[];
 };
-
-const testimonialItems = players.slice(0, 5).map((player) => ({
-  image: player.image,
-  name: player.name,
-  date: player.game,
-}));
 
 const SponsorsContent = ({ sponsors }: SponsorsContentProps) => (
   <>
@@ -21,7 +15,6 @@ const SponsorsContent = ({ sponsors }: SponsorsContentProps) => (
     <div className="sponsor-section padding-top">
       <div className="container">
         <SponsorGrid items={sponsors} />
-        <TestimonialSection items={testimonialItems} />
       </div>
     </div>
   </>
