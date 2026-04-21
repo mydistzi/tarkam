e91fff8af97b99d52a96a2165ea25a7507ee9bd3
mydistzi <mysurellinux@gmail.com>
2026-04-21 23:56:21 +0700

    fix: update Content-Type header from 'text/xml' to 'application/xml' for sitemap responses

--- api/sitemap.js
+++ api/sitemap.js
@@ -3,7 +3,7 @@ import { generateSitemapXml } from '../lib/sitemap.js';
 export default async function handler(req, res) {
   try {
     const sitemap = await generateSitemapXml();
-    res.setHeader('Content-Type', 'text/xml; charset=utf-8');
+    res.setHeader('Content-Type', 'application/xml; charset=utf-8');
     res.setHeader('Cache-Control', 'public, max-age=300');
     res.status(200).send(sitemap);
   } catch (error) {
