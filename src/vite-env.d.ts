f5338110e0cf2f36401a03847335a99bcc6d091a
mydistzi <mysurellinux@gmail.com>
2026-04-23 00:04:20 +0700

    feat: update swiper imports and clean up unused declarations

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="vite/client" />
 declare const __APP_BUILD_ID__: string;
 declare module "swiper/css";
-declare module "swiper/css/navigation";
 declare module "swiper/css/effect-coverflow";
 declare module "swiper/css/pagination";
-declare module "swiper/css/thumbs";
-declare module "swiper/css/free-mode";
 declare module "*.js";
